0f0c19301ee1971379df90760a7ef56ed934c977
Matthew <mthunter22@gmail.com>
2020-09-25 13:23:03 -0400

    final

--- src/main/java/com/fizzbuzz/app/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/app/FizzBuzz.java
@@ -11,33 +11,24 @@ public class FizzBuzz
         String lucky = "lucky";
         String result = "";
         String space = " ";
-        int fizzCount = 0, buzzCount = 0, fizzBuzzCount = 0, luckyCount = 0, integerCount = 0;
         for(int number : rangeOfNumbers) {
             if (Integer.toString(number).contains("3")) {
-                result = result + lucky + space;
-                luckyCount += 1;
+                result += lucky + space;
             }
             else if (number % 15 == 0) {
                 result += fizzbuzz + space;
-                fizzBuzzCount += 1;
             }
             else if (number % 3 == 0) {
                 result += fizz + space;
-                fizzCount += 1;
             }
             else if (number % 5 == 0) {
                 result += buzz + space;
-                buzzCount += 1;
             }
             else {
                 result += Integer.toString(number) + space;
-                integerCount += 1;
             }
         }
-        String counts = "\n" + "fizz: " + Integer.toString(fizzCount) + "\n" + "buzz: " + Integer.toString(buzzCount) + "\n" + "fizzbuzz: " + Integer.toString(fizzBuzzCount) + "\n" + "lucky: " + Integer.toString(luckyCount) + "\n" + "integer: " + Integer.toString(integerCount);
-        result = result + counts;
         System.out.println(result);
         return result;
     }
 }
-
