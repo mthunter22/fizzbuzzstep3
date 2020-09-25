9837414b55eaf12cddeb0b7764a254a7f4b779de
Matthew <mthunter22@gmail.com>
2020-09-25 13:25:20 -0400

    final

--- src/main/java/com/fizzbuzz/app/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/app/FizzBuzz.java
@@ -3,32 +3,30 @@ package com.fizzbuzz.app;
 public class FizzBuzz
 {
     public static void main( String[] args ) { }
-
     public static String transpose(int[] rangeOfNumbers) {
         String fizz = "fizz";
         String buzz = "buzz";
         String fizzbuzz = "fizzbuzz";
-        String lucky = "lucky";
         String result = "";
         String space = " ";
         for(int number : rangeOfNumbers) {
-            if (Integer.toString(number).contains("3")) {
-                result += lucky + space;
-            }
-            else if (number % 15 == 0) {
+            if (number % 15 == 0) {
+                System.out.print(fizzbuzz + " ");
                 result += fizzbuzz + space;
             }
             else if (number % 3 == 0) {
+                System.out.print(fizz + " ");
                 result += fizz + space;
             }
             else if (number % 5 == 0) {
+                System.out.print(buzz + " ");
                 result += buzz + space;
             }
             else {
+                System.out.print(number + " ");
                 result += Integer.toString(number) + space;
             }
         }
-        System.out.println(result);
         return result;
     }
 }
